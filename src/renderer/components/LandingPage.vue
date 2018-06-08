75ce649f61e7e7d3c1fc26b018c4595fcdab22b0
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2018-06-08 20:49:55 +0200

    Add first informations got with youtube package

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -18,14 +18,25 @@
           <div class="column"></div>
           <div class="column">
             <div class="ui fluid action input">
-              <input type="text" placeholder="YouTube URL">
-              <button class="ui red right labeled icon button">
+              <input type="text" placeholder="YouTube URL" v-model="link">
+              <button class="ui red right labeled icon button" @click="getFile()">
                 <i class="music icon"></i>
                 Convert
               </button>
             </div>
           </div>
         </div>
+      </div>
+      <div class="ui three column grid videoInfo" v-if="info">
+          <div class="row">
+            <div class="column"></div>
+            <div class="column">
+              <p><sui-image :src="info.thumbnail" size="medium"/></p>
+              <p>Title : {{info.title}}</p>
+              <p>Duration : {{info.duration}}</p>
+              <p>Likes : {{info.like_count}} <i class="red heart icon"></i></p>
+            </div>
+          </div>
       </div>
       <div class="ui inverted vertical footer segment form-page">
         <div class="ui container">
@@ -37,36 +48,52 @@
 </template>
 
 <script>
+  const youtubedl = require('youtube-dl');
   export default {
     name: 'landing-page',
+    data() {
+      return {
+        link: null,
+        info: null,
+      };
+    },
     methods: {
-      open(link) {
-        this.$electron.shell.openExternal(link);
+      getFile() {
+        youtubedl.getInfo(this.link, null, (err, info) => {
+          if (err) throw err;
+          console.log(info);
+
+          this.info = info;
+        });
       },
     },
   };
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro");
 
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Source Sans Pro", sans-serif;
+  background-color: #445260;
+}
+
+.videoInfo {
+    color: #FFFFFF;
+}
+
+@media (min-height: 320px) {
+  .ui.footer.form-page {
+    /* Increased specificity for SO snippet priority */
+    position: absolute;
+    bottom: 0;
+    width: 100%;
   }
-
-  body {
-    font-family: 'Source Sans Pro', sans-serif;
-    background-color: #445260;
-  }
-
-  @media (min-height: 320px) {
-    .ui.footer.form-page { /* Increased specificity for SO snippet priority */
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-    }
-  }
-
+}
 </style>
