eeb48c025fb4b49c6291029740b5779a0ea87f98
unknown <bonjour@xavierchop.in>
2018-06-20 23:37:59 +0200

    Read settings file and added it globally

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -8,6 +8,13 @@ import App from './App';
 import router from './router';
 import store from './store';
 
+const remote = window.require('electron').remote;
+const electronFs = remote.require('fs');
+
+const settings = electronFs.readFileSync('settings.json');
+
+global.sharedObj = settings;
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
