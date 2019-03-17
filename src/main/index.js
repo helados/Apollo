0d4825b5e39d27c9ac4bb0004ecf7c982ff4ff31
xchopin <xchopin54@gmail.com>
2019-03-17 17:03:57 +0100

    style: comment tools

--- src/main/index.js
+++ src/main/index.js
@@ -27,7 +27,7 @@ function createWindow() {
 
   mainWindow.loadURL(winURL);
 
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', () => {
     mainWindow = null;
