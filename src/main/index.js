1e862547ffffb3ae5624fe3dd2493871908319fa
Xavier Chopin <bonjour@xavierchop.in>
2019-03-24 17:52:37 +0100

    fix: disable console

--- src/main/index.js
+++ src/main/index.js
@@ -26,7 +26,7 @@ function createWindow() {
   });
 
   mainWindow.loadURL(winURL);
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
 
   mainWindow.on('closed', () => {
